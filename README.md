a8d8b4103992e467709cef7e0fd891fc96d070fc
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2022-06-08 09:32:37 -0400

    2D arrays

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-c66648af7eb3fe8bc4f294546bfd86ef473780cde1dea487d3c4ff354943c9ae.svg)](https://classroom.github.com/online_ide?assignment_repo_id=7931423&assignment_repo_type=AssignmentRepo)
-# Spaceman (edit this)
+# Back to Earth (edit this)
 
-Travel in a spaceship 
+Travel in a spaceship and manuever your way back to Earth without crashing into any aliens.
--- Sketch1.java
+++ Sketch1.java
@@ -1,15 +1,21 @@
 import processing.core.PApplet;
-import processing.core.PImage;
+
 
 public class Sketch1 extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+	//Variables for the playing screen 
+  int cellSize = 40;
+  int rowNum = 12;
+  int columnNum = 20;
+  int screenHeight;
+  int screenWidth;
+
   public void settings() {
-	// put your size call here
-    size(800, 500);
+	 
+   //calculatint the size of the screen
+   screenHeight = rowNum * cellSize;
+   screenWidth = columnNum * cellSize;
+    size(screenWidth, screenHeight);
   }
 
   /** 
@@ -17,17 +23,38 @@ public class Sketch1 extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
+
     background(210, 255, 173);
+ 
   }
 
+ //include Pics/
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  PImage imgMenu;
-    imgMenu = loadImage("");
-    //include Pics/
+    int levelData [] []= {
+      {3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 6},
+      {1, 19, 26, 26, 22, 0, 19, 26, 26, 26, 26, 26, 26, 22, 0, 19, 26, 26, 22, 4},
+      {1, 21, 0, 0, 21, 0, 21, 0, 0, 0, 0, 0, 0, 21, 0, 21, 0, 0, 21, 4},
+      {1, 21, 0, 19, 24, 26, 16, 26, 26, 18, 18, 26, 26, 16, 26, 24, 22, 0, 21, 4},
+      {1, 21, 0, 21, 0, 0, 21, 0, 0, 101, 101, 0, 0, 21, 0, 0, 21, 0, 21, 4},
+      {1, 17, 26, 16, 26, 26, 20, 0, 99, 100, 100, 100, 0, 17, 26, 26, 16, 26, 20, 4},
+      {1, 21, 0, 21, 0, 0, 21, 0, 0, 0, 0, 0, 0, 21, 0, 0, 21, 0, 21, 4},
+      {1, 21, 0, 25, 18, 26, 16, 26, 26, 26, 26, 26, 26, 16, 26, 18, 28, 0, 21, 4},
+      {1, 21, 0, 0, 21, 0, 21, 0, 0, 0, 0, 0, 0, 21, 0, 21, 0, 0, 21, 4},
+      {1, 25, 26, 26, 28, 0, 25, 26, 26, 26, 26, 26, 26, 28, 0, 25, 26, 26, 28, 4},
+      {9, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 12},
+    };
+  
+    
   }
+    //0 = rectangle, 1 = left border, 2 = top border, 4 = right border, 8= bottom border, 16 = pellets
+  
+  
   
   // define other methods down here.
+
+  
+ 
 }
