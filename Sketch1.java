e90e71a108f8d9ee5fe823907b5a8a2ecf3927d2
CherylL23 <Cheryl.Lee23@ycdsbk12.ca>
2022-06-14 22:21:08 -0400

    timer, menu, endings, map design finished

--- Sketch1.java
+++ Sketch1.java
@@ -11,15 +11,21 @@ public class Sketch1 extends PApplet {
   int screenHeight;
   int screenWidth;
 
-  boolean menu = true;
+  //boolean values for screen switching
+  boolean drawMenu;
+  boolean drawMap;
+  boolean drawEnd1;
+  boolean drawEnd2;
 
-  boolean drawMenu = true;
-  boolean drawMap = false;
-  boolean drawEnd1 = false;
-  boolean drawEnd2 = false;
- 
   
-
+  
+  
+  
+  
+  
+  
+  
+  
   public void settings() {
 	 
    //calculating the size of the screen
@@ -34,41 +40,77 @@ public class Sketch1 extends PApplet {
    */
   public void setup() {
 
+    drawMenu = true;
     background(13, 37, 145);
+   
   }
 
- 
+  public void draw() {
 
+    //using if() to figure out which screen is needed while playing th game
+    if(drawMenu){
+     
+      drawMenu();
+    }
+
+    else if(drawMap) {
+     
+     drawMap();
+     Timer();
+    }
+
+    else if(drawEnd1) {
+     drawEnd1();
+    }
+
+    else if(drawEnd2) {
+      drawEnd2();
+    }
+ }
  
 
   public void drawMenu(){
+
+    //menu screen
     PImage starsGround = loadImage("Pics/BackgroundStars.jpg");
     starsGround.resize(800, 480); 
     image(starsGround, 0, 0);
+
+    PImage Earth = loadImage("Pics/BigEarth.png");
+    Earth.resize(600, 600);
+    image(Earth, 400, 300);
+
+    PImage Sat = loadImage("Pics/satellite.png");
+    Sat.resize(50,50);
+    image(Sat, 600, 200);
+
+    PImage Rocket = loadImage("Pics/rocket.png");
+    Rocket.resize(200,200);
+    image(Rocket, 60, 130);
+
+    fill(247, 249, 252);
+    textSize(60);
+    text("Back to Earth", 235, 200);
     textSize(40); 
-    text("Press s!", 300, 300);
+    text("Press s to start!", 260, 300);
 
-    if(keyPressed){
-      if(key == 's'){
-       drawMap = true;
-       drawMenu = false;
-       
-      }
- 
-     }
-
-  
-
-    
-   
-    
+    if(key == 's'){
+     drawMap = true;
+     drawMenu = false;
+    } 
   }
 
   public void drawMap() {
+
+    //playing level screen
     background(13, 37, 145);
     PImage mapBackground = loadImage("Pics/BackgroundStars.jpg");
     mapBackground.resize(800, 435); 
     image(mapBackground, 0, 0);
+
+    PImage astroP = loadImage("Pics/astronaut.png");
+    astroP.resize(40, 40);
+    image(astroP, 0, 440);
     
     
     int levelData [] [] = {
@@ -96,19 +138,17 @@ public class Sketch1 extends PApplet {
        if(cell == 1) {
          PImage moonBlock = loadImage("Pics/moonBlock.png"); 
          moonBlock.resize(40, 40);
-         //image(moonBlock, (column * 40 - column) + 10 , (row * 40  - row) + 5);
          image(moonBlock, column * cellSize, row * 40);
         }
 
        if(cell == 0){
          noStroke();
          fill(251, 255, 13);
-         //ellipse((column * 40 - column) + 30, (row * 40 - row) + 25, 20, 20);
          ellipse((column * 40) + 20, (row * 40) + 20 , 20, 20);
         }
 
         if (cell == 3){
-         PImage Earth = loadImage("Pics/BigEarth.png");
+          PImage Earth = loadImage("Pics/BigEarth.png");
           Earth.resize(40, 40);
           image(Earth, (column * 40), (row * 40));
         } 
@@ -117,63 +157,114 @@ public class Sketch1 extends PApplet {
           noStroke();
           fill(184, 111, 17);
           rect((column * 40) , (row * 40), 40, 40);
-        }
-
-        //int secondTicks = second();
-
-       
+        }     
       }
     }
-
-  
-    
-   
- 
-     
-    
-      
-  
   } 
  
+  private final long createdMillis = System.currentTimeMillis();
   public void Timer() {
-    //int secondTicks = 60;
-    for(int secondTicks = second(); secondTicks > 0; secondTicks++){
-      secondTicks++;
-      
-      fill(125);
-      textSize(30);
-      text(secondTicks, 760, 470);
-      
+  
+    //counts current time in milliseconds and converts to seconds. 
+    long nowMillisecond = System.currentTimeMillis();
+    int nowSecond = (int) (nowMillisecond -createdMillis) / 1000;
+    fill(125);
+    textSize(30);
+    text(nowSecond, 760, 470);
+
+    //Based on a certain time, you get certain endings of the game
+    if (nowSecond < 20){
+      fill(237, 234, 69);
+      textSize(15);
+      text("<(Use arrows keys to navigate the spaceship.Collect all of the fuel to go back to Earth)", 45, 460);
+      text("**Hurry! you only have 60sec!!", 45, 474);
     }
 
-  }
-   
-  public void draw() {
-
-
-    if(drawMenu = true){
-      drawMenu();
-    }
-
-    else if(drawMap = true) {
-      
-      drawMap();
-      Timer();
+    if (nowSecond > 30 && nowSecond < 35){
+      fill(237, 234, 69);
+      textSize(15);
+      text("<(less than 30 seconds left... Speed up!)", 45, 460);
       
     }
 
-    else if(drawEnd1 = true) {
-
+    if (nowSecond > 50 && nowSecond < 56){
+      fill(237, 234, 69);
+      textSize(15);
+      text("<(The spaceship is breaking apart.. oxygen is depleting...)", 45, 460);
     }
 
-    else if(drawEnd2 = true) {
-
+    if (nowSecond == 58 || nowSecond == 59 || nowSecond == 57){
+      fill(237, 234, 69);
+      textSize(15);
+      text("<(Is this the end?)", 45, 460);
     }
-
     
+    if(nowSecond == 60) {
+      drawEnd1 = true;
+      drawMap = false;
+    }
   }
   
+  public void drawEnd1() {
 
+    //bad end
+    background(145, 41, 20);
+    fill(242, 238, 237);
+    textSize(40);
+    text("You are trapped in space and died suffocation.", 20, 80);
+
+    PImage xFace = loadImage("Pics/melt.png");
+    xFace.resize(200, 200);
+    image(xFace, 300, 150);
+
+    fill(242, 238, 237);
+    textSize(30);
+    text("'Press r to go back to the main menu'", 160, 420);
+
+    //return to menu screen
+    if(keyPressed){
+      if(key == 'r'){
+        drawMenu = true;
+        drawEnd1 = false;
+      }
+    }
+  }
+
+  public void drawEnd2 () {
+
+    //Happy end
+    background(62, 114, 237);
+    fill(0);
+    textSize(30);
+    text("The spaceship landed on earth safely. Yay!", 20, 200);
+
+    textSize(20);
+    text("'Press r to go back to the main menu'", 20, 250);
+  
+    PImage landRock = loadImage("Pics/clipart63877.png");
+    landRock.resize(150, 300);
+    image(landRock, 600, 130);
+
+    PImage astroJump = loadImage("Pics/PinClipart.com_astronaut-clipart_311716.png");
+    astroJump.resize(100, 100);
+    image(astroJump, 520, 335);
+
+    noStroke();
+    fill(33, 102, 50);
+    rect(0, 430, 800, 800);
+
+    noStroke();
+    fill(235, 228, 40);
+    ellipse(50, 60, 80, 80);
+
+    //return to menu screen
+    if(keyPressed){
+      if(key == 'r'){
+        drawMenu = true;
+        drawEnd1 = false;
+      }
+    }
+  }
   
  
 }
